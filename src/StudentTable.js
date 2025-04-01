343a5ebeb1136b0362a1148dcce73b36a2c0330d
Taylor Masiello <masiellotay@gmail.com>
2025-04-01 09:21:16 -0400

    added + utilized state (useState hook); made studentData dynamic + added additional student info to API endpoint; renders all studentData from API when studentData exists

--- src/StudentTable.js
+++ src/StudentTable.js
@@ -1,16 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function StudentTable(){
+    const [students, setStudents] = useState("");
+
     useEffect(()=>{
         fetch('http://localhost:8000/students')
         .then((res)=>res.json())
-        .then((data)=>{
-            console.log(data);
-        })
-        .catch((err)=>{
-            console.log(err.message);
-        })
+        .then((data)=>setStudents(data))
+        .catch((err)=>console.log(err.message))
     },[])
 
     return(
@@ -30,18 +28,20 @@ export default function StudentTable(){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Taylor Masiello</td>
-                            <td>Clearwater, FL</td>
-                            <td>fakeemail@gmail.com</td>
-                            <td>123-456-7890</td>
+                        {students && students.map((item)=>(
+                            <tr>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.location}</td>
+                            <td>{item.email}</td>
+                            <td>{item.phone}</td>
                             <td>
                                 <a href="" className="btn btn-info">View</a>
                                 <a href="" className="btn btn-primary">Edit</a>
                                 <a href="" className="btn btn-danger">Delete</a>
                             </td>
-                        </tr>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
